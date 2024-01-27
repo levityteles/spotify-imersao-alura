a73f47b0fcbccdc50ce19277369d41511c68092f
levityteles <paulotelesbatos@outlook.com>
2024-01-26 22:06:26 -0300

    att

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
+const searchInput = document.getElementById("search-input");
 const resultArtist = document.getElementById("result-artist");
 const playlistContainer = document.getElementById("result-playlists");
-const searchInput = document.getElementsByClassName("section1");
+
+console.log(searchInput.value);
 
 function requestApi(searchTerm) {
   fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
     .then((response) => response.json())
-    .then((results) => displayResults(results));
+    .then((result) => displayResults(result))
 }
 
-function displayResults(results) {
-  hidePlaylists();
+function displayResults(result) {
+  // hidePlaylists();
+  playlistContainer.classList.add("hidden");
   const artistImage = document.getElementById("artist-img");
   const artistName = document.getElementById("artist-name");
 
-  results.forEach((element) => {
-    artistImage.src = element.urlImg;
+  result.forEach((element) => {
     artistName.innerText = element.name;
+    artistImage.src = element.urlImg;
   });
-  resultArtist.classList.remove("hidden");
+  resultArtist.classList.remove('hidden');
 }
 
-function hidePlaylists() {
-  playlistContainer.classList.add("hidden");
-}
+// function hidePlaylists() {
+//   playlistContainer.classList.add("hidden");
+// }
 
-searchInput.addEventListener("input", function () {
+document.addEventListener("input", function () {
   const searchTerm = searchInput.value.toLowerCase();
   if (searchTerm === "") {
-    resultArtist.classList.add("hidden");
     playlistContainer.classList.remove("hidden");
+    resultArtist.classList.add("hidden");
     return;
   }
   requestApi(searchTerm);
 })
+
 // COLORS CSS **********************************************************************
 
 document.querySelector("#close").addEventListener("click", function() {
